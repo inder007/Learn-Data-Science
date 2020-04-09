9815bf3612d9523804534cc2f73a34cb837ec557
Inderpreet Singh Chera <inderchera@gmail.com>
2020-04-10 00:03:07 +0530

    Tried running docker command from java app

---
+++ javaExecTest/ProcessBuilderTest.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ProcessBuilderTest{
+    public static void main(String[] args) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("/bin/bash", "-c", "sudo docker run --rm -it sample bash /usr/src/script.sh /usr/src/run.py");
+        // processBuilder.command("/bin/bash", "-c", "sudo ls /etc");
+        try {
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            StringBuilder output = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                output.append(line + "\n");
+            }
+
+            int exitVal = process.waitFor();
+            if (exitVal == 0) {
+                System.out.println("Success!");
+                System.out.println(output);
+                System.exit(0);
+            }
+            else{
+                System.out.println("Error");
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+}
--- newVersionTest/src/main/java/com/example/LearnDataScience/resources/FormSubmit.java
+++ newVersionTest/src/main/java/com/example/LearnDataScience/resources/FormSubmit.java
@@ -1,7 +1,9 @@
 package com.example.LearnDataScience.resources;
 
+import java.io.BufferedReader;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -28,9 +30,23 @@ public class FormSubmit {
                 System.out.print(code.charAt(i));
             }
             fout.close();
-            return code;
+            String check = "docker run -v /home/joker/project/newVersionTest/codes/"+filename+":/usr/src/run.py --rm -it sample bash /usr/src/script.sh /usr/src/run.py";
+//            String command = "sudo cd /home/joker/project/dockerTest &&  docker run -v /home/joker/project/newVersionTest/codes/"+filename+":/usr/src/run.py --rm -it sample bash /usr/src/script.sh /usr/src/run.py && cd /home/joker/project/newVersionTest";
+//            String command = "sudo ls ; ls";
+//            Process proc = Runtime.getRuntime().exec(command);
+            Process proc = Runtime.getRuntime().exec(new String[]{"su", "-c", check});
+            BufferedReader reader = new BufferedReader(new InputStreamReader((proc.getInputStream())));
+            String line = "";
+            StringBuffer out = new StringBuffer();
+            while((line = reader.readLine()) != null){
+//                System.out.print(line + "\n");
+                out.append(line);
+            }
+            int val = proc.waitFor();
+            System.out.println(val);
+            return out.toString();
         }
-        catch(IOException e){
+        catch(IOException | InterruptedException e){
             System.out.println(e);
             return "Error";
         }
