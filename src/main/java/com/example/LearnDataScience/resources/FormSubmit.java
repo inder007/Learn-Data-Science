fdc5775fcec28cf0cb8ced147044d0afbe813d35
inder007 <joker@Inderpreets-MacBook-Pro.local>
2020-06-10 20:48:22 +0530

    Corrected Error Handling

--- src/main/java/com/example/LearnDataScience/resources/FormSubmit.java
+++ src/main/java/com/example/LearnDataScience/resources/FormSubmit.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
 
 
 @Path("/formSubmit")
@@ -13,9 +14,14 @@ import javax.ws.rs.core.Response;
 public class FormSubmit {
 
     @POST
-    public String uploadCode(String code){
+    public Response uploadCode(String code){
         CodeRunner codeRunner = new CodeRunner(code);
-        String output = codeRunner.PythonCodeRunner();
-        return output;
+        try {
+            String output = codeRunner.PythonCodeRunner();
+            return Response.ok(output.toString()).build();
+        }
+        catch (IOException | InterruptedException e){
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
